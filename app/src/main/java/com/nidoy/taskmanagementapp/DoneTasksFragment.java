9a91697260450472633c56499c27d6d24cfc7e3a
Roy <royallen.nidoy.iics@ust.edu.ph>
2020-09-24 01:28:20 +0800

    added TabLayout and PagerAdapter for TasksFragment

---
+++ app/src/main/java/com/nidoy/taskmanagementapp/DoneTasksFragment.java
@@ -0,0 +1,25 @@
+package com.nidoy.taskmanagementapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class DoneTasksFragment extends Fragment {
+
+    public DoneTasksFragment() { /* Required empty public constructor */ }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_done_tasks, container, false);
+    }
+}
