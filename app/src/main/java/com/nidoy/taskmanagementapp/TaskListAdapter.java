b3531fd7828bbadcf99be6c0fb4a046e38f694fb
Roy Allen <royallen.nidoy.iics@ust.edu.ph>
2020-10-03 18:24:24 +0800

    arranged dependencies in gradle file

--- app/src/main/java/com/nidoy/taskmanagementapp/TaskListAdapter.java
+++ app/src/main/java/com/nidoy/taskmanagementapp/TaskListAdapter.java
@@ -42,21 +42,20 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.TaskVi
     @Override
     public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
         Task current = mTasks.get(position);
-
         holder.txtLabel.setText(current.getmLabel());
         holder.txtDueDate.setText(new SimpleDateFormat("MMM. dd, yyyy").format(current.getmDue()));
         holder.txtTime.setText(new SimpleDateFormat("hh:mm aa").format(current.getmDue()));
         holder.txtNotes.setText(current.getmNotes());
         holder.txtNotes.setVisibility(TextUtils.isEmpty(current.getmNotes()) ? View.GONE : View.VISIBLE);
+        holder.btnExpand.setOnClickListener(v -> {
+            holder.expanded.setVisibility(holder.expanded.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
+            holder.btnExpand.setImageResource(holder.expanded.getVisibility() == View.GONE ? R.drawable.ic_expand_more_black_24dp : R.drawable.ic_expand_less_black_24dp);
+        });
         holder.btnEdit.setOnClickListener(v -> ((Activity) mContext).startActivityForResult(new Intent(mContext, TaskActivity.class).putExtra("task", current), MainActivity.UPDATE_TASK_ACTIVITY_REQUEST_CODE));
         holder.btnDelete.setOnClickListener(v -> new AlertDialog.Builder(mContext).setMessage(mContext.getText(R.string.prompt_delete_task)).setPositiveButton(mContext.getText(R.string.yes), (dialog, which) -> {
             MainActivity.mTaskViewModel.delete(current);
             Snackbar.make(holder.itemView, mContext.getString(R.string.snack_task_deleted), Snackbar.LENGTH_SHORT).show();
         }).setNegativeButton(mContext.getText(R.string.no), null).create().show());
-        holder.btnExpand.setOnClickListener(v -> {
-            holder.expanded.setVisibility(holder.expanded.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
-            holder.btnExpand.setImageResource(holder.expanded.getVisibility() == View.GONE ? R.drawable.ic_expand_more_black_24dp : R.drawable.ic_expand_less_black_24dp);
-        });
     }
 
     @NonNull
@@ -85,7 +84,6 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.TaskVi
         private final ImageButton btnDelete, btnEdit, btnExpand;
         private final MaterialCardView parent;
         private final TextView txtDueDate, txtLabel, txtNotes, txtTime;
-
         private TaskViewHolder(View itemView) {
             super(itemView);
             // UI elements
